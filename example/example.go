8b67692a7cb90bd600b2fbe167646019cf717f9e
Raffaele Sena <raff367@gmail.com>
2013-06-29 13:47:31 -0700

    Improved example: - added quit/exit command - added prompt command

--- example/example.go
+++ example/example.go
@@ -3,20 +3,38 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"github.com/gobs/readline"
 )
 
 func main() {
 	prompt := "by your command> ";
 
-	//loop until ReadLine returns nil (signalling EOF)
-	L: for {
-		switch result := readline.ReadLine(&prompt); true {
-		case result == nil: break L //exit loop
+	// loop until ReadLine returns nil (signalling EOF)
 
-		case *result != "": //ignore blank lines
-			fmt.Println(*result);
-			readline.AddHistory(*result); //allow user to recall this line
+	L: for {
+		result := readline.ReadLine(&prompt);
+		if result == nil { // exit loop
+			break L
+		}
+
+		line := *result
+
+		switch line {
+		case "": // ignore blank lines
+			continue
+
+		case "exit", "quit":
+			break L
+
+		default:
+			if strings.HasPrefix(line, "prompt ") {
+				prompt = strings.TrimPrefix(line, "prompt ")
+			} else {
+				fmt.Println(line);
+			}
+
+			readline.AddHistory(line); //allow user to recall this line
 		}
 	}
 }
